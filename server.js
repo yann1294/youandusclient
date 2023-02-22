3ae47512091b07d70e7830bf04b721735a224fb1
yann1294 <dev.yann12@gmail.com>
2023-02-22 23:15:15 +0530

    change proxy middleware

--- server.js
+++ server.js
@@ -10,14 +10,19 @@ const handle = app.getRequestHandler();
 
 const csrfMiddleware = async (req, res, next) => {
   try {
-    const response = await fetch('https://lobster-app-gvavo.ondigitalocean.app/api/csrf-token', { credentials: 'same-origin' });
+    const response = await fetch('https://lobster-app-gvavo.ondigitalocean.app/api/csrf-token', { credentials: 'include' });
     const { csrfToken } = await response.json();
     req.headers['x-csrf-token'] = csrfToken;
+
+    // Set the Access-Control-Allow-Origin header to the specific origin that is allowed to access the resource
+    res.setHeader('Access-Control-Allow-Origin', 'https://youandusclient.vercel.app');
+
     next();
   } catch (error) {
     next(error);
   }
 };
+
 
 app.prepare().then(() => {
   const server = express();
