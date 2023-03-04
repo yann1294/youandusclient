55c3fcd965c7b0c229b0e652c0ec047b23a12f1d
yann1294 <36128975+yann1294@users.noreply.github.com>
2023-03-05 04:27:34 +0530

    cors update client

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://youandusclient.vercel.app,https://youandus-server.vercel.app" },
+          { key: "Access-Control-Allow-Origin", value: "https://youandusclient.vercel.app/" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
